e60fb4624bde9947e1e7f704e04b32ad0c61394b
viniciusmiradouro <miradouro.v@gmail.com>
2023-08-01 11:47:11 -0300

    Changed match cases in quicksort back to if statements
    The function becomes more readable and concise in this way.

--- functional_python_algorithms/sorts/quicksort.py
+++ functional_python_algorithms/sorts/quicksort.py
@@ -1,8 +1,6 @@
-def quicksort(t: tuple) -> tuple:
-    match t:
-        case (pivot, *rest):
-            minor_partition = tuple(x for x in rest if x <= pivot)
-            greater_partition = tuple(x for x in rest if x > pivot)
-            return quicksort(minor_partition) + (pivot,) + quicksort(greater_partition)
-        case _:
-            return t
+def quicksort(t: tuple[int, ...]) -> tuple[int, ...]:
+    if not t or len(t) == 1:
+        return t
+    minor_partition = tuple(x for x in t[1:] if x <= t[0])
+    greater_partition = tuple(x for x in t[1:] if x > t[0])
+    return quicksort(minor_partition) + (t[0],) + quicksort(greater_partition)
