0b8055b467f08ada85b5964b57aba7458b0f2726
viniciusmiradouro <miradouro.v@gmail.com>
2023-08-01 10:43:20 -0300

    Changed quicksort implementation to use pattern matching

--- functional_python_algorithms/sorts/quicksort.py
+++ functional_python_algorithms/sorts/quicksort.py
@@ -1,8 +1,8 @@
 def quicksort(t: tuple) -> tuple:
-    if t == () or len(t) == 1:
-        return t
-    elif len(t) == 2:
-        return (t[0], t[1]) if t[0] <= t[1] else (t[1], t[0])
-    minor_partition = tuple(filter(lambda x: x <= t[0], t[1:]))
-    greater_partition = tuple(filter(lambda x: x >= t[0], t[1:]))
-    return quicksort(minor_partition) + (t[0],) + quicksort(greater_partition)
+    match t:
+        case (pivot, *rest):
+            minor_partition = tuple(x for x in rest if x <= pivot)
+            greater_partition = tuple(x for x in rest if x > pivot)
+            return quicksort(minor_partition) + (pivot,) + quicksort(greater_partition)
+        case _:
+            return t
