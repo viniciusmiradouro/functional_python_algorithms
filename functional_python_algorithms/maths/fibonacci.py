2e0af21fcb0d6ca8e98996daaa039c935bb71b25
viniciusmiradouro <miradouro.v@gmail.com>
2023-08-02 14:54:28 -0300

    Fixed fibonacci

--- functional_python_algorithms/maths/fibonacci.py
+++ functional_python_algorithms/maths/fibonacci.py
@@ -2,4 +2,4 @@ from pydantic import NonNegativeInt as Natural
 
 
 def fibonacci(x: Natural) -> Natural:
-    return 1 if x < 3 else fibonacci(x - 2) + fibonacci(x - 3)
+    return 1 if x < 3 else fibonacci(x - 2) + fibonacci(x - 1)
