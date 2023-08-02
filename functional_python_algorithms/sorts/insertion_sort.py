831f169f67bcb2752ada76284dc4fbdcc6da2dc1
viniciusmiradouro <miradouro.v@gmail.com>
2023-08-02 10:27:04 -0300

    Fixed insertion sort
    The recursive call of the inner function insert was leaving behind part of the input tuple.

--- functional_python_algorithms/sorts/insertion_sort.py
+++ functional_python_algorithms/sorts/insertion_sort.py
@@ -1,8 +1,8 @@
 def insertion_sort(t: tuple[int, ...]) -> tuple[int, ...]:
 
     def insert(x: int, t: tuple[int, ...]) -> tuple[int, ...]:
-        if not t or x <= t[0]:
+        if not t or x < t[0]:
             return (x,) + t
-        return insert(x, t[1:])
+        return (t[0],) + insert(x, t[1:])
 
     return t if not t else insert(t[0], insertion_sort(t[1:]))
