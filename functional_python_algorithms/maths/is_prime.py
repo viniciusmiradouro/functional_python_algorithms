1a5870652dab846f78d36855cdb1efd255e84dbb
viniciusmiradouro <miradouro.v@gmail.com>
2023-08-02 12:56:09 -0300

    Fixed is_prime

--- functional_python_algorithms/maths/is_prime.py
+++ functional_python_algorithms/maths/is_prime.py
@@ -1,2 +1,2 @@
 def is_prime(x: int) -> bool:
-    return False if x < 2 else not any(map(lambda d: x % d == 0, range(2, x + 1)))
+    return False if -2 < x < 2 else not any(map(lambda d: x % d == 0, range(2, abs(x))))
